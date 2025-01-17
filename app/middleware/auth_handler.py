e89282f82dc76527625dafda9170e6f0e7f67b7d
Josia Silalahi <130029208+silalahi20@users.noreply.github.com>
2025-01-17 11:07:32 +0700

    feat : add authorize scheme for swagger

--- app/middleware/auth_handler.py
+++ app/middleware/auth_handler.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 from typing import Optional
 from jose import jwt
 from fastapi import HTTPException, Depends
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer,HTTPBearer, HTTPAuthorizationCredentials
 from passlib.context import CryptContext
 from app.config.database import get_collection
 from bson import ObjectId
@@ -12,7 +12,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-
+security = HTTPBearer()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM =os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES",30))
@@ -33,21 +33,20 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -
 async def get_user(user_id: str):
     users_collection = get_collection("users")
     user = await users_collection.find_one({"_id": ObjectId(user_id)})
-    return user if user else None
+    return user
 
-async def get_current_user(token: str = Depends(oauth2_scheme)):
+async def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
     try:
+        token = credentials.credentials
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        user_id = payload.get("sub")
+        user_id: str = payload.get("sub")
         if not user_id:
-            raise HTTPException(status_code=401, detail="Invalid token")
+            raise HTTPException(status_code=401, detail="Invalid token: Missing user ID")
         user = await get_user(user_id)
         if not user:
             raise HTTPException(status_code=404, detail="User not found")
-        
-        # Konversi _id menjadi string sebelum mengembalikan user
         user["_id"] = str(user["_id"])
         return user
-    except jwt.JWTError as e:
-        raise HTTPException(status_code=401, detail=f"Invalid token: {str(e)}")
+    except jwt.JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
 
